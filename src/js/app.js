58c4d8901d9ebeb5d025c4d44b5f26fbfc19cb03
Rita Akhmadulina <rita.akhmadulina@gmail.com>
2016-08-30 17:10:26 +0300

    mobile-mnu

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
 
+  // hamburger-menu
+  $('#nav-icon').click(function(){
+    //$(this).toggleClass('open');
+    //console.log(':::>',pizda);
+  });
+
+  // swiper JS
   var swiper = '.swiper-container';
 
   new Swiper (swiper + '.is-small', {
@@ -15,7 +22,22 @@ $(document).ready(function() {
     loop: true,
     slidesPerView: 4,
     nextButton: '.arrow-button-next',
-    prevButton: '.arrow-button-prev'
+    prevButton: '.arrow-button-prev',
+    breakpoints: {
+      // when window width is <= 320px
+      1024: {
+        slidesPerView: 3,
+        spaceBetweenSlides: 30
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetweenSlides: 20
+      },
+      416: {
+        slidesPerView: 1,
+        spaceBetweenSlides: 10
+      }
+    }
   });
 
   new Swiper (swiper + '.is-general', {
@@ -27,6 +49,7 @@ $(document).ready(function() {
     effect: 'fade'
   });
 
+  // events
   $('.cart').click (function () {
     $('.js-popup').addClass('is-visible');
     $('.overlay').addClass('is-visible');
@@ -38,6 +61,21 @@ $(document).ready(function() {
 
   $('.currency').click (function () {
     $('.js-currency').addClass('is-visible');
+  });
+
+  $('.hamburger').click (function () {
+    $('.mobile-menu').addClass('is-visible');
+  });
+  $('.js-close2').click (function () {
+    $('.mobile-menu').removeClass('is-visible');
+  });
+  $('.js-search').click (function () {
+    $('.form.is-search').addClass('is-open');
+  });
+  $(document).click(function (event) {
+    if ($(event.target).closest('.js-search').length == 0 && $(event.target).attr('id') != 'toggle-link') {
+      $('.input.is-small').hide();
+    }
   });
 
 
